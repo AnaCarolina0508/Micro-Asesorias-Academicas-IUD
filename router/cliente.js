d3ee0b33bb2f2d4a5681be300d75c5199b9390f0
Ana Carolina <ana.gonzalez@est.iudigital.edu.co>
2022-11-01 18:24:35 -0500

    metodo_put

--- router/cliente.js
+++ router/cliente.js
@@ -1,18 +1,73 @@
 const { Router } = require('express'); 
 const router = Router();
-const cliente = require('../models/Cliente');
+const Cliente = require('../models/Cliente');
 
-router.get('/', function(req, res){
-    res.send('Estoy desde cliente GET');
+router.get('/',async function(req, res){
+    try {
+        const clientes = await Cliente.find();
+        res.send(clientes);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error');
+    }
 });
 
-router.post('/', function(req, res){
-   console.log('Objeto recibido', req.body);
-   res.send(req.body);
+
+router.post('/', async function(req, res){
+    try{
+        
+        console.log('Objeto recibido', req.body);
+
+        const existeCliente = await Cliente.findOne({email: req.body.email});
+        console.log(existeCliente);
+        if (existeCliente){
+            return res.status(400).send('Email ya existe');
+        }
+
+        let cliente = new Cliente();
+        cliente.nombre = req.body.nombre;
+        cliente.email = req.body.email;
+        cliente.fechaCreacion = new Date();
+        cliente.fechaActualizacion = new Date();
+
+        cliente = await cliente.save();
+        
+        res.send(cliente);
+    } catch (error){
+        console.log(error);
+        res.status(500).send('ocurrió un error');
+    }
 });
 
-router.put('/', function(req, res){
-    res.send('Estoy desde cliente PUT');
+router.put('/:clienteId', async function(req, res){
+    try {
+        console.log(req.body, req.params);
+
+        let cliente = await Cliente.findById(req.params.clienteId );
+        
+        if (!cliente) {
+            return res.status(400).send('Cliente no existe');
+        }
+
+        const existeCliente = await Cliente
+            .findOne({ email: req.body.email, _id:{$ne:cliente._id} });
+        console.log(existeCliente);
+        if (existeCliente) {
+            return res.status(400).send('Email ya existe');
+        }
+
+        cliente.email = req.body.email;
+        cliente.nombre = req.body.nombre;
+        cliente.fechaActualizacion = new Date();
+            
+
+        cliente = await cliente.save();
+
+        res.send(cliente);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrió un error');
+    }
 });
 
 module.exports = router;
