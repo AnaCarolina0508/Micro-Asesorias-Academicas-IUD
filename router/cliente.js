c394a1e1caefe3a897b07f0d1ba39ed66a561e02
Ana Carolina <ana.gonzalez@est.iudigital.edu.co>
2022-11-01 18:51:29 -0500

    get_put_post_etapas

--- router/cliente.js
+++ router/cliente.js
@@ -1,26 +1,26 @@
-const { Router } = require('express'); 
+const { Router } = require('express');
 const router = Router();
 const Cliente = require('../models/Cliente');
 
-router.get('/',async function(req, res){
+router.get('/', async function (req, res) {
     try {
         const clientes = await Cliente.find();
         res.send(clientes);
     } catch (error) {
         console.log(error);
-        res.status(500).send('Ocurrio un error');
+        res.status(500).send('Ocurrió un error');
     }
 });
 
 
-router.post('/', async function(req, res){
-    try{
-        
+router.post('/', async function (req, res) {
+    try {
+
         console.log('Objeto recibido', req.body);
 
-        const existeCliente = await Cliente.findOne({email: req.body.email});
+        const existeCliente = await Cliente.findOne({ email: req.body.email });
         console.log(existeCliente);
-        if (existeCliente){
+        if (existeCliente) {
             return res.status(400).send('Email ya existe');
         }
 
@@ -31,26 +31,26 @@ router.post('/', async function(req, res){
         cliente.fechaActualizacion = new Date();
 
         cliente = await cliente.save();
-        
+
         res.send(cliente);
-    } catch (error){
+    } catch (error) {
         console.log(error);
         res.status(500).send('ocurrió un error');
     }
 });
 
-router.put('/:clienteId', async function(req, res){
+router.put('/:clienteId', async function (req, res) {
     try {
         console.log(req.body, req.params);
 
-        let cliente = await Cliente.findById(req.params.clienteId );
-        
+        let cliente = await Cliente.findById(req.params.clienteId);
+
         if (!cliente) {
             return res.status(400).send('Cliente no existe');
         }
 
         const existeCliente = await Cliente
-            .findOne({ email: req.body.email, _id:{$ne:cliente._id} });
+            .findOne({ email: req.body.email, _id: { $ne: cliente._id } });
         console.log(existeCliente);
         if (existeCliente) {
             return res.status(400).send('Email ya existe');
@@ -59,7 +59,7 @@ router.put('/:clienteId', async function(req, res){
         cliente.email = req.body.email;
         cliente.nombre = req.body.nombre;
         cliente.fechaActualizacion = new Date();
-            
+
 
         cliente = await cliente.save();
 
