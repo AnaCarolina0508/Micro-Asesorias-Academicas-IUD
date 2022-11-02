a678f44786f234c95d7bb275a4430e568e50cc40
Ana Carolina <ana.gonzalez@est.iudigital.edu.co>
2022-11-01 21:28:02 -0500

    get_put_post_proyecto

--- router/proyecto.js
+++ router/proyecto.js
@@ -1,17 +1,88 @@
-const { Router } = require('express'); 
-
+const { Router } = require('express');
+const Proyecto = require('../models/Proyecto');
 const router = Router();
 
-router.get('/', function(req, res){
-    res.send('Estoy desde proyecto GET');
+router.get('/', async function (req, res) {
+    try {
+        const proyectos = await Proyecto.find().populate([
+            {
+                path: 'etapas', select: 'nombre'
+            },
+            {
+                path: 'cliente', select: 'nombre email'
+            },
+            {
+                path: 'universidad', select: 'nombre direccion telefono'
+            },
+            {
+                path: 'tipoProyecto', select: 'nombre'
+            }
+        ]);
+        res.send(proyectos);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error al consultar proyectos');
+    }
 });
+router.post('/', async function (req, res) {
+    try {
+        
+        const existeProyectoPorNumero = await Proyecto.findOne({ numero: req.body.numero });
+        if (existeProyectoPorNumero) {
+            return res.status(400).send('Ya existe el numero para otro proyecto');
+        }
+        
+        let proyecto = new Proyecto();
+        proyecto.numero = req.body.numero;
+        proyecto.titulo = req.body.titulo;
+        proyecto.fechaIniciacion = req.body.fechaIniciacion;
+        proyecto.fechaEntrega = req.body.fechaEntrega;
+        proyecto.fechaCreacion = new Date();
+        proyecto.fechaActualizacion = new Date();
+        proyecto.valor = req.body.valor;
+        proyecto.cliente = req.body.cliente._id;
+        proyecto.tipoProyecto = req.body.tipoProyecto._id;
+        proyecto.universidad = req.body.universidad._id;
+        proyecto.etapas = req.body.etapas._id;
+              
+        proyecto = await proyecto.save();
+        res.send(proyecto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error');
+    }
 
-router.post('/', function(req, res){
-    res.send('Estoy desde proyecto POST');
 });
+router.put('/:proyectoId', async function (req, res) {
+    try {
+        let proyecto = await Proyecto.findById(req.params.proyectoId);
+        if (!proyecto) {
+            return res.status(400).send('Proyecto no existe');
+        }
 
-router.put('/', function(req, res){
-    res.send('Estoy desde proyecto PUT');
+        const existeProyectoPorNumero = await Proyecto
+            .findOne({ numero: req.body.numero, _id:{$ne: proyecto._id } });
+        if (existeProyectoPorNumero) {
+            return res.status(400).send('Ya existe el numero para otro proyecto');
+        }
+
+        proyecto.numero = req.body.numero;
+        proyecto.titulo = req.body.titulo;
+        proyecto.fechaIniciacion = req.body.fechaIniciacion;
+        proyecto.fechaEntrega = req.body.fechaEntrega;
+        proyecto.fechaActualizacion = new Date();
+        proyecto.valor = req.body.valor;
+        proyecto.cliente = req.body.cliente._id;
+        proyecto.tipoProyecto = req.body.tipoProyecto._id;
+        proyecto.universidad = req.body.universidad._id;
+        proyecto.etapas = req.body.etapas._id;
+
+        proyecto = await proyecto.save();
+        res.send(proyecto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error al consultar proyectos');
+    }
 });
 
 module.exports = router;
