6aad7e7be199aa3d630ef4220f920d09b339d055
JUAN CAMILO JIENEZ VASQUEZ <juan.jimenezv@est.iudigital.edu.co>
2022-11-09 22:22:00 -0500

    modifico index.js

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ getConnection();
 
 //Parseo JSON
 app.use(express.json());
-
+app.use( '/cliente', require('./router/cliente') );
+app.use( '/universidad', require('./router/universidad') );
+app.use( '/etapas', require('./router/etapas') );
+app.use( '/tipoProyecto', require('./router/tipoProyecto') );
 app.use( '/proyecto', require('./router/proyecto') );
 
 
